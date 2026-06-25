9c22b8208b532288d5bd4cbb90d5530dca4a6861
Oscarrrrrrrrrrrrrrrr <oscaryavat3@gmail.com>
2026-06-25 13:34:21 +0700

    Delete Target-Web directory

--- Target-Web/vulnerabilities/weak_id/source/impossible.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$html = "";
-
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-	$cookie_value = bin2hex(random_bytes(20));
-	setcookie("dvwaSession", $cookie_value, time()+3600, "/vulnerabilities/weak_id/", $_SERVER['HTTP_HOST'], true, true);
-}
-?>
